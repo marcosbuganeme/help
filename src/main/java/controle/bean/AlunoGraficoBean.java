f9f5ca97a9e1f2dcaf3990f281f440559e41549e
marcos olavo silva buganeme <marcos.after@gmail.com>
2015-02-27 23:54:31 -0300

    xhtml
    Layout em html e xml
    -> Padronização de icones
    -> Melhoria no layout
    -> Modificação na nota de rodapé, add link para o git do desenvolvedor

--- src/main/java/controle/bean/AlunoGraficoBean.java
+++ src/main/java/controle/bean/AlunoGraficoBean.java
@@ -51,54 +51,55 @@ public class AlunoGraficoBean implements Serializable {
 		this.modeloGraficoLinha = this.preencherDadosGrafico();
 		this.modeloGraficoLinha.setTitle("Quantidade de cursos vendidos por mês");
 		this.modeloGraficoLinha.setLegendPosition("e");
+		this.modeloGraficoLinha.setAnimate(true);
 		this.modeloGraficoLinha.setShowPointLabels(true);
 		this.modeloGraficoLinha.getAxes().put(AxisType.X, new CategoryAxis("Meses"));
 		final Axis yAxis = this.modeloGraficoLinha.getAxis(AxisType.Y);
 		yAxis.setLabel("Quantidade de cursos");
 		yAxis.setMin(0);
-		yAxis.setMax(20);
+		yAxis.setMax(25);
 	}
 
 	private LineChartModel preencherDadosGrafico() {
 
 		final LineChartModel grafico = new LineChartModel();
 
-		final ChartSeries javaOO = new ChartSeries();
-		javaOO.setLabel("Java Orientado a Objetos");
-		javaOO.set(new String("Novembro - 2014"), 1);
-		javaOO.set(new String("Dezembro - 2014"), 4);
-		javaOO.set(new String("Janeiro - 2015"), 5);
-		javaOO.set(new String("Fevereiro - 2015"), 3);
-		javaOO.set(new String("Março - 2015"), 2);
+		final ChartSeries backEnd = new ChartSeries();
+		backEnd.setLabel("BEND");
+		backEnd.set(new String("Novembro - 2014"), new Double(1.0));
+		backEnd.set(new String("Dezembro - 2014"), 2);
+		backEnd.set(new String("Janeiro - 2015"), 3);
+		backEnd.set(new String("Fevereiro - 2015"), 4);
+		backEnd.set(new String("Março - 2015"), 5);
 
-		final ChartSeries javaEE = new ChartSeries();
-		javaEE.setLabel("Java EE");
-		javaEE.set(new String("Novembro - 2014"), 4);
-		javaEE.set(new String("Dezembro - 2014"), 5);
-		javaEE.set(new String("Janeiro - 2015"), 12);
-		javaEE.set(new String("Fevereiro - 2015"), 9);
-		javaEE.set(new String("Março - 2015"), 6);
+		final ChartSeries frontEnd = new ChartSeries();
+		frontEnd.setLabel("FEND");
+		frontEnd.set(new String("Novembro - 2014"), 3);
+		frontEnd.set(new String("Dezembro - 2014"), 5);
+		frontEnd.set(new String("Janeiro - 2015"), 7);
+		frontEnd.set(new String("Fevereiro - 2015"), 9);
+		frontEnd.set(new String("Março - 2015"), 11);
 
-		final ChartSeries logicaProgramacao = new ChartSeries();
-		logicaProgramacao.setLabel("Lógica de programação");
-		logicaProgramacao.set(new String("Novembro - 2014"), 6);
-		logicaProgramacao.set(new String("Dezembro - 2014"), 5);
-		logicaProgramacao.set(new String("Janeiro - 2015"), 5);
-		logicaProgramacao.set(new String("Fevereiro - 2015"), 4);
-		logicaProgramacao.set(new String("Março - 2015"), 3);
+		final ChartSeries dba = new ChartSeries();
+		dba.setLabel("DBA");
+		dba.set(new String("Novembro - 2014"), 5);
+		dba.set(new String("Dezembro - 2014"), 9);
+		dba.set(new String("Janeiro - 2015"), 13);
+		dba.set(new String("Fevereiro - 2015"), 17);
+		dba.set(new String("Março - 2015"), 21);
 
-		final ChartSeries infraestrutura = new ChartSeries();
-		infraestrutura.setLabel("Infra estrutura");
-		infraestrutura.set(new String("Novembro - 2014"), 2);
-		infraestrutura.set(new String("Dezembro - 2014"), 1);
-		infraestrutura.set(new String("Janeiro - 2015"), 4);
-		infraestrutura.set(new String("Fevereiro - 2015"), 2);
-		infraestrutura.set(new String("Março - 2015"), 1);
+		final ChartSeries infra = new ChartSeries();
+		infra.setLabel("INFRA");
+		infra.set(new String("Novembro - 2014"), 8);
+		infra.set(new String("Dezembro - 2014"), 7);
+		infra.set(new String("Janeiro - 2015"), 6);
+		infra.set(new String("Fevereiro - 2015"), 5);
+		infra.set(new String("Março - 2015"), 4);
 
-		grafico.addSeries(javaOO);
-		grafico.addSeries(javaEE);
-		grafico.addSeries(infraestrutura);
-		grafico.addSeries(logicaProgramacao);
+		grafico.addSeries(backEnd);
+		grafico.addSeries(dba);
+		grafico.addSeries(frontEnd);
+		grafico.addSeries(infra);
 
 		return grafico;
 	}
